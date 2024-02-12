11a3f9002aa0dbee3cf1ae8cf6582b4ca7dc3780
AnastasiiaTkachuk <anastaisha.580@gmail.com>
2024-02-12 22:20:26 +0100

    Add Publisher and PublisherRepository

---
+++ src/main/java/com/anastasiia/spring6webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.anastasiia.spring6webapp.repositories;
+
+import com.anastasiia.spring6webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
