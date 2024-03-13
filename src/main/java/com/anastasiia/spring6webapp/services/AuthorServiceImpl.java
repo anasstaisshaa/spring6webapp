2a4897a801be100fad81cd0ea374b345a33f1bf0
AnastasiiaTkachuk <anastaisha.580@gmail.com>
2024-03-13 10:17:37 +0100

    fixed mistakes

--- src/main/java/com/anastasiia/spring6webapp/services/AuthorServiceImpl.java
+++ src/main/java/com/anastasiia/spring6webapp/services/AuthorServiceImpl.java
@@ -2,7 +2,9 @@ package com.anastasiia.spring6webapp.services;
 
 import com.anastasiia.spring6webapp.domain.Author;
 import com.anastasiia.spring6webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthorServiceImpl implements AuthorService {
 
     private final AuthorRepository authorRepository;
